fd6bdd3e318a8986cb4253ef1df5dcb87dc267cd
ayushpandey78001-creator <ayushpandey78001@gmail.com>
2026-06-30 21:20:59 +0530

    Time: 0 ms (100%), Space: 10.2 MB (63.24%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    // Function to reverse the order of words in a string
+    // Function to reverse the order of words 
     string reverseWords(string s) {
-        // Vector to store words
-        vector<string> words;
-        
-        // Temporary string to store a single word
-        string word = "";
-        
-        // Traverse the string character by character
-        for (int i = 0; i < s.size(); i++) {
-            // If the character is not a space, add it to the current word
-            if (s[i] != ' ') {
-                word += s[i];
-            } 
-            // If we encounter a space and have a word collected
-            else if (!word.empty()) {
-                // Push the collected word to words list
-                words.push_back(word);
-                // Reset word for next word collection
-                word = "";
-            }
-        }
-        
-        // Push the last word if it exists
-        if (!word.empty()) {
-            words.push_back(word);
-        }
-        
-        // Reverse the list of words
-        reverse(words.begin(), words.end());
-        
-        // Join the words into a single string separated by spaces
+        // Result string to store final output
         string result = "";
-        for (int i = 0; i < words.size(); i++) {
-            result += words[i];
-            // Add a space if it's not the last word
-            if (i < words.size() - 1) {
+        
+        // Pointer starting from the last character
+        int i = s.size() - 1;
+        
+        // Traverse string from right to left
+        while (i >= 0) {
+            // Skip spaces at the current position
+            while (i >= 0 && s[i] == ' ') {
+                i--;
+            }
+            
+            // If pointer is out of bounds, break
+            if (i < 0) break;
+            
+            // Mark the end of the current word
+            int end = i;
+            
+            // Move left until a space or start of string is found
+            while (i >= 0 && s[i] != ' ') {
+                i--;
+            }
+            
+            // Extract the current word
+            string word = s.substr(i + 1, end - i);
+            
+            // Add space before appending next word if result is not empty
+            if (!result.empty()) {
                 result += " ";
             }
+            
+            // Append the word to the result
+            result += word;
         }
         
         return result;
