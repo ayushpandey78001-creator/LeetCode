a442867c3ce4f6c193aeca2ca94034588eef9150
ayushpandey78001-creator <ayushpandey78001@gmail.com>
2026-07-03 10:46:30 +0530

    Time: 0 ms (100%), Space: 52.2 MB (83.49%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // Function to determine if you can reach the last index
+    bool canJump(vector<int>& nums) {
+        int maxIndex = 0; // The farthest index we can currently reach
+
+        // Traverse the array
+        for (int i = 0; i < nums.size(); i++) {
+            // If current index is beyond the farthest reachable point
+            if (i > maxIndex) {
+                return false; // We cannot move further
+            }
+
+            // Update the farthest index we can reach
+            maxIndex = max(maxIndex, i + nums[i]);
+        }
+
+        // If we finish the loop, we can reach the last index
+        return true;
+    }
+};
