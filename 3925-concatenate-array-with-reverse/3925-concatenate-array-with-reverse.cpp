546b27aeba31f9f7e1adb4b3eb87e9b093a4776b
ayushpandey78001-creator <ayushpandey78001@gmail.com>
2026-07-10 23:56:48 +0530

    Time: 0 ms (100%), Space: 31.2 MB (7.61%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<int> concatWithReverse(vector<int>& nums) {
-        vector<int> rev = nums; 
-        reverse(rev.begin(), rev.end()); // Modifies rev in place
-        nums.insert(nums.end(), rev.begin(), rev.end()); // Faster than back_inserter
+        // Appends elements from back to front directly into nums
+        nums.insert(nums.end(), nums.rbegin(), nums.rend()); 
         return nums;
     }
 };
