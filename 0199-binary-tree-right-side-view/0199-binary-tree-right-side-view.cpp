14f706295162694d90626c8a04d73a01066ddb05
ayushpandey78001-creator <ayushpandey78001@gmail.com>
2026-08-26 23:06:21 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.75%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,41 +11,37 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (!root) return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> level;
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                level.push_back(node->val);
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-            ans.push_back(level);
-        }
-        return ans;
+    void leftDFS(TreeNode* node, int level, vector<int>& res) {
+        if (!node) return;
+
+        if (res.size() == level)
+            res.push_back(node->val);
+
+        leftDFS(node->left, level + 1, res);
+
+        leftDFS(node->right, level + 1, res);
+    }
+
+    void rightDFS(TreeNode* node, int level, vector<int>& res) {
+        if (!node) return;
+
+        if (res.size() == level)
+            res.push_back(node->val);
+
+        rightDFS(node->right, level + 1, res);
+
+        rightDFS(node->left, level + 1, res);
     }
 
     vector<int> leftView(TreeNode* root) {
-        vector<vector<int>> levels = levelOrder(root);
-        vector<int> left;
-        for (auto& level : levels) {
-            left.push_back(level[0]);
-        }
-        return left;
+        vector<int> res;
+        leftDFS(root, 0, res);
+        return res;
     }
 
     vector<int> rightSideView(TreeNode* root) {
-        vector<vector<int>> levels = levelOrder(root);
-        vector<int> right;
-        for (auto& level : levels) {
-            right.push_back(level.back());
-        }
-        return right;
+        vector<int> res;
+        rightDFS(root, 0, res);
+        return res;
     }
 };
